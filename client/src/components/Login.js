c9b4a7f211ef2af79d4ce33b9696ddaa887a92ac
Jin Woo Kim <jinwookim@Jins-MacBook-Air.local>
2021-04-03 15:31:22 +1100

    login page done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import '../App.css';
-import Wave from 'react-wavify';
 import Button from '@material-ui/core/Button';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 import {
     BrowserRouter as Router,
@@ -10,16 +24,89 @@ import {
     Link,
 } from 'react-router-dom';
 
-export default function () {
+const useStyles = makeStyles((theme) => ({
+    abRoot: {
+      flexGrow: 1,
+      backgroundcolor: "red",
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+  
+  export default function MenuAppBar() {
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+  
+    const handleChange = (event) => {
+      setAuth(event.target.checked);
+    };
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
     return (
         <div> 
-            <div id="banner">
-                <span class="menuButton">Profile</span>
-                <span>My Timetables</span>
-                <span>About us</span>
-                <span>Logout</span>
+         <div className={classes.root}>
+    
+            <AppBar position="static">
+                <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                    Prolvan Timetabling
+                </Typography>
+                <Button variant="h9" className={classes.title} style={{maxWidth: '150px'}}>
+                    My Timetables
+                </Button>
+                <Button variant="h9" className={classes.title} style={{maxWidth: '150px'}}>
+                    About us
+                </Button>
+                {auth && (
+                    <div>
+                    <IconButton
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    {/* <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                        }}
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        
+                    </Menu> */}
+                    </div>
+                )}
+                </Toolbar>
+            </AppBar>
+         </div>
+         <Fab color="secondary" aria-label="add" id="addTableButton">
+            <AddIcon />
+         </Fab>
 
-            </div>
-        </div>
-    );   
-}
+    </div>
+    );
+  }
+
