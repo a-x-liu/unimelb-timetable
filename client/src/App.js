a8456a884c83aba3a0a19af25091de75b63c0013
a-x-liu <60502157+a-x-liu@users.noreply.github.com>
2021-04-03 22:45:23 +1100

    Revert "update"

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 import Register from './components/Register';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Timetables from './components/Timetables';
-import About from './components/About';
 
 import {
   BrowserRouter as Router,
@@ -17,9 +13,6 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-import { lightBlue } from '@material-ui/core/colors';
-import {  makeStyles } from'@material-ui/core/styles';
-import Wave from 'react-wavify';
 
 function Alert (props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -38,38 +31,29 @@ function LoginSection () {
 
   const username = {
     marginRight: '5px',
-    width: '90%',
-    marginBottom: '2.5%'
+    width: '210px'
   }
 
   const password = {
-    width: '90%',
-    height: '120%'
+    width: '210px'
   }
-
 
   async function submitLogin (e) {
     console.log("we are submitting");
   }
 
   return (
-    <div id="container">
-      <div id="info"> 
-        <h2> Prolvan Timetabling</h2>
-        <div> This is a timetabling service brought to you by Prolvan and co!</div>
-      </div>
-      <div className="loginPage">
+    <div>
+      <div className="loginPage" id="section">
         <h2>Login</h2>
         <form onSubmit={submitLogin}>
-          <TextField id="loginUsername" label="Username" variant="outlined" style={username}/>
+          <TextField id="loginEmail" label="Email" variant="outlined" style={username}/>
           <TextField id="loginPassword" label="Password" variant="outlined" type="password" style={password} />
           <div className="loginButton">
-            <Link to="/login" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="primary">
-                Login
-              </Button>
-            </Link>
-            <Link to="/register" style={{ textDecoration: 'none' }}>
+            <Button variant="contained" color="primary" type="submit">
+              Login
+            </Button>
+            <Link to="/register">
               <Button variant="contained" color="secondary">
                 Register
               </Button>
@@ -77,18 +61,12 @@ function LoginSection () {
           </div>
         </form>
       </div>
-      <div id="wave">
-        <Wave fill='#D3EAFF'
-          paused={false}
-          options={{
-            position: 'fixed',
-            bottom: '0px',
-            height: 20,
-            amplitude: 35,
-            speed: 0.2,
-            points: 4
-          }}
-        />
+      <div>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            This is a success message!
+          </Alert>
+        </Snackbar>
       </div>
     </div>
   );
@@ -103,16 +81,10 @@ function App () {
           <Register />
         </Route>
         <Route path="/login">
-          <Login />
+          <LoginSection />
         </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/timetables">
-          <Timetables />
+        <Route path="/dashboard">
+          <LoginSection />
         </Route>
         <Route path="/">
           <LoginSection />
